78a0f3b713f404ae526e6f1b68659efca9894454
scott88lee@gmail.com <scott88lee@gmail.com>
2021-08-01 14:43:35 +0800

    update variablename

--- models/webhooks.js
+++ models/webhooks.js
@@ -5,11 +5,11 @@ module.exports = {
       let id = jsonBody.id;      
       console.log(id)
       let d = new Date();
-      let dat = d.toUTCString();
+      let timestamp = d.toUTCString();
 
 
       try {
-         let result = await db.query('INSERT INTO shopify_webhook_orders(shopify_order_id, raw_data, created_at) VALUES($1, $2, $3)', [id, jsonBody, dat])
+         let result = await db.query('INSERT INTO shopify_webhook_orders(shopify_order_id, raw_data, created_at) VALUES($1, $2, $3)', [id, jsonBody, timestamp])
          return result;
       } catch (err) {
          console.log(err)
