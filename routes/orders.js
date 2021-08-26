9e47bbe02574d2ebf11d5d1f41fd531a8b1e9fbf
scott88lee@gmail.com <scott88lee@gmail.com>
2021-08-27 00:30:53 +0800

    Clean up

--- routes/orders.js
+++ routes/orders.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const ordersController = require('../controllers/orders');
 
 // ROUTES
-router.get('/', ordersController.getall);
 router.get('/archive', ordersController.listArchive);
 
 router.post('/post/webhook', ordersController.postWebhook);
