b5a0d4017125b208a4b88aa7c56b1b0ec5c9b294
scott88lee <scott88lee@gmail.com>
2021-08-27 20:19:54 +0800

    testing queued process

--- controllers/webHooks.js
+++ controllers/webHooks.js
@@ -1,4 +1,5 @@
-const webhooks = require('../models/webhooks.js')
+const webhooks = require('../models/webhooks.js');
+const axios = require('axios');
 
 module.exports = {
     shopifyOrderCreate: async (req, res) => {
@@ -6,7 +7,19 @@ module.exports = {
         let body = req.body;
         console.log("Recieving webhook: " + JSON.stringify(body));
         
-        let result =  await webhooks.archiveWebhook(body);
-        res.send(result);
+        let savedWebhook =  await webhooks.archiveWebhook(body);
+        
+        if (savedWebhook) {
+            console.log("Before process")
+            this.processOrder(body);
+            console.log("After process")
+            res.send("200 OK");
+        }
+    },
+
+    processOrder : (JSONBody) => {
+        console.log("Start of process")
+        let jsn = axios.get("https://bridge.lenskart.com/api/sg/prescriptions/85888838");
+        console.log(jsn);
     }
 }
