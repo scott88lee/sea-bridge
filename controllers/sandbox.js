939faea904ad10b544660579bcfdc83243c4921e
scott88lee@gmail.com <scott88lee@gmail.com>
2021-09-27 11:04:00 +0800

    Create sandbox

--- controllers/sandbox.js
+++ controllers/sandbox.js
@@ -3,5 +3,11 @@ const db = require('../config/db')
 module.exports = {
    index: (req, res) => {
       res.render("packages/new")
+   },
+
+   upload: (req, res) => {
+      console.log(req.body)
+      console.log(req)
+      res.send(req)
    }
 }
--- routes/sandbox.js
+++ routes/sandbox.js
@@ -4,5 +4,7 @@ const controller = require('../controllers/sandbox');
 
 // ROUTES
 router.get('/', controller.index);
+router.post('/upload', controller.upload);
+
 
 module.exports = router;
