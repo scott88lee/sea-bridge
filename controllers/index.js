416d1bc8ad16e84d024589c493decf7b7b7b514d
scott88lee@gmail.com <scott88lee@gmail.com>
2021-09-29 13:46:20 +0800

    Clean up useless code

--- controllers/index.js
+++ controllers/index.js
@@ -2,12 +2,7 @@ const db = require('../config/db')
 
 module.exports = {
     main: (req, res) => {
-        let str = "SELECT * FROM fruits;"
-        db.query( str, (err, result) => {
-            console.log(result)
-            res.redirect("/orders")
-            //res.send("Welcome to SEA-Bridge")
-        })
+        res.redirect("/orders/archive")
     },
 
     test: async (req, res) => {
